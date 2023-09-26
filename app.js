14dddd4777c47a9b8c26ec480b2c5c61d4107768
fraulueneburg <github@fraulueneburg.de>
2023-09-26 18:04:00 +0200

    added hbs helper

--- app.js
+++ app.js
@@ -1,39 +1,57 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv').config();
+require('dotenv').config()
 
 // ℹ️ Connects to the database
-require('./db');
+require('./db')
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require('express')
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
+const hbs = require('hbs')
 
-const app = express();
+hbs.registerHelper('ifequal', function (lvalue, rvalue, options) {
+	if (arguments.length < 3) throw new Error('Handlebars Helper equal needs 2 parameters')
+	if (lvalue != rvalue) {
+		return options.inverse(this)
+	} else {
+		return options.fn(this)
+	}
+})
+
+hbs.registerHelper('ifnotequal', function (lvalue, rvalue, options) {
+	if (arguments.length < 3) throw new Error('Handlebars Helper equal needs 2 parameters')
+	if (lvalue == rvalue) {
+		return options.inverse(this)
+	} else {
+		return options.fn(this)
+	}
+})
+
+const app = express()
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require('./config')(app);
+require('./config')(app)
 
 // default value for title local
-const capitalize = require('./utils/capitalize');
-const projectName = 'SO FRESH SO CLEAN';
+const capitalize = require('./utils/capitalize')
+const projectName = 'SO FRESH SO CLEAN'
 
-app.locals.appTitle = `${projectName}`;
+app.locals.appTitle = `${projectName}`
 
 // 👇 Start handling routes here
-const indexRoutes = require('./routes/index.routes');
-app.use('/', indexRoutes);
+const indexRoutes = require('./routes/index.routes')
+app.use('/', indexRoutes)
 
 // User route
-app.use('/', require('./routes/user.routes'));
-app.use('/', require('./routes/flat.routes'));
-app.use('/', require('./routes/task.routes'));
+app.use('/', require('./routes/user.routes'))
+app.use('/', require('./routes/flat.routes'))
+app.use('/', require('./routes/task.routes'))
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require('./error-handling')(app)
 
-module.exports = app;
+module.exports = app
