ded856e25308e766e32d706b08af8644eaaf28df
fraulueneburg <github@fraulueneburg.de>
2023-09-26 18:03:35 +0200

    improve syntax

--- config/index.js
+++ config/index.js
@@ -1,75 +1,73 @@
 // We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+const express = require('express')
 
-const session = require("express-session");
+const session = require('express-session')
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+const logger = require('morgan')
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs')
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser')
 
 // ℹ️ Serves a custom favicon on each request
 // https://www.npmjs.com/package/serve-favicon
-const favicon = require("serve-favicon");
+const favicon = require('serve-favicon')
 
 // ℹ️ global package used to `normalize` paths amongst different operating systems
 // https://www.npmjs.com/package/path
-const path = require("path");
+const path = require('path')
 
-const MongoStore = require("connect-mongo");
-const { MONGO_URI } = require("../db");
+const MongoStore = require('connect-mongo')
+const { MONGO_URI } = require('../db')
 
 // Middleware configuration
 module.exports = (app) => {
-  // In development environment the app logs
-  app.use(logger("dev"));
+	// In development environment the app logs
+	app.use(logger('dev'))
 
-  // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+	// To have access to `body` property in the request
+	app.use(express.json())
+	app.use(express.urlencoded({ extended: false }))
+	app.use(cookieParser())
 
-  app.use(
-    session({
-      secret: process.env.SESS_SECRET,
-      store: MongoStore.create({ mongoUrl: MONGO_URI }),
-      resave: true,
-      saveUninitialized: false,
+	app.use(
+		session({
+			secret: process.env.SESS_SECRET,
+			store: MongoStore.create({ mongoUrl: MONGO_URI }),
+			resave: true,
+			saveUninitialized: false,
 
-      cookie: {
-        // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        // secure: process.env.NODE_ENV === 'production',
-        sameSite: "lax",
-        secure: false,
-        httpOnly: true,
-        maxAge: 600000, // 60 * 1000 * 10 ms === 10 min
-      },
-    })
-  );
+			cookie: {
+				// sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+				// secure: process.env.NODE_ENV === 'production',
+				sameSite: 'lax',
+				secure: false,
+				httpOnly: true,
+				maxAge: 600000, // 60 * 1000 * 10 ms === 10 min
+			},
+		})
+	)
 
-  app.use((req, res, next) => {
-    res.locals.userFromSession = req.session.user;
-    next();
-  });
+	app.use((req, res, next) => {
+		res.locals.userFromSession = req.session.user
+		next()
+	})
 
-  // Normalizes the path to the views folder
-  app.set("views", path.join(__dirname, "..", "views"));
-  // access to the handlebars partials folder
-  hbs.registerPartials(path.join(__dirname, "..", "views/partials"));
-  // Sets the view engine to handlebars
-  app.set("view engine", "hbs");
-  // Handles access to the public folder
-  app.use(express.static(path.join(__dirname, "..", "public")));
+	// Normalizes the path to the views folder
+	app.set('views', path.join(__dirname, '..', 'views'))
+	// access to the handlebars partials folder
+	hbs.registerPartials(path.join(__dirname, '..', 'views/partials'))
+	// Sets the view engine to handlebars
+	app.set('view engine', 'hbs')
+	// Handles access to the public folder
+	app.use(express.static(path.join(__dirname, '..', 'public')))
 
-  // Handles access to the favicon
-  app.use(
-    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
-  );
-};
+	// Handles access to the favicon
+	app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
+}
